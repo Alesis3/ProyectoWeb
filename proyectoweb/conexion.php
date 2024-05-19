bd84930c896e3e9d65bb2cf6e89cf0c878c13a2f
Alesis3 <alexis.art349@hotmail.com>
2024-05-19 15:42:26 -0600

    Commit9

--- proyectoweb/conexion.php
+++ proyectoweb/conexion.php
@@ -1,8 +1,12 @@
 <?php
-$host_db="127.0.0.1:3306";
-$user_db="Alexis Arteaga";
-$pass_db="13579";
-$db_name="B_Datos_proyecto";
+$host_db="127.0.0.1";
+$user_db="root";
+$pass_db="Appdlks-03";
+$db_name="Pro";
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
 ?>
