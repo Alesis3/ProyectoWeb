a3413c97f8bfa5fd77a94b3b639d8a356b69c436
Alesis3 <alexis.art349@hotmail.com>
2024-05-20 13:56:31 -0600

    commit11

--- proyectoweb/conexion.php
+++ proyectoweb/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host_db="127.0.0.1";
 $user_db="root";
-$pass_db="Appdlks-03";
-$db_name="Pro";
+$pass_db="";
+$db_name="proyectoWeb";
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
 
